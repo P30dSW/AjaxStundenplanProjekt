e65f6b4eff5c2b7b2173874419fb7754ccc6a88a
Pedro <30877908+P30dSW@users.noreply.github.com>
2018-09-27 15:36:36 +0200

    working AjaxUnitTest

--- Stundenplan.QUnitTest/js/ajaxScriptTest.js
+++ Stundenplan.QUnitTest/js/ajaxScriptTest.js
@@ -1,12 +1,15 @@
+/*Good to know https://code.tutsplus.com/tutorials/how-to-test-your-javascript-code-with-qunit--net-9077
+   https://api.qunitjs.com/assert/timeout
+*/
+
 QUnit.test("getLaborListTEST", function(a){
-    /*Good to know https://code.tutsplus.com/tutorials/how-to-test-your-javascript-code-with-qunit--net-9077
-     https://api.qunitjs.com/assert/timeout
-     */
-     stop(1000);
-    getLaborList(function(){
-        a.ok(true);
-    });
-    setTimeout(function() {
-        start();
-    }, 2000);
+    var done = a.async();
+    getLaborList().done(function(json) {
+        if(typeof json, "object"){
+            a.ok(true,"Der Webservice Response is ein Objekt");
+        }else{
+            a.ok(false,"Der Webservice Response is kein Objekt");
+        }
+        done();
+      });
 });
